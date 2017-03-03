f4bcb74ee78809e8599730d618cc9a7dde366640
chiho828 <chiho828@uw.edu>
2017-03-02 19:28:26 -0800

    test if the program works

--- getmem.c
+++ getmem.c
@@ -17,7 +17,6 @@
 #include "mem_impl.h"
 
 // Function prototypes
-FreeBlock* create(FreeBlock* block, uintptr_t size);
 FreeBlock* split(FreeBlock* block, uintptr_t size);
 
 // Return pointer to a memory block with at least size of given parameter
@@ -35,91 +34,101 @@ void* getmem(uintptr_t size) {
 
     alignSize = alignSize < TH ? TH : alignSize;
 
-    FreeBlock* current = NULL;
+    FreeBlock* current;
     if (!freeList) {
-      if (alignSize > REQ_SIZE) { // Default size not big enough
-	current = create(current, alignSize); // Get larger chunk from OS
-      } else {
-	current = create(current, REQ_SIZE); // Get 8K bytes memory from OS
-	freeList = split(current, alignSize); // Split block if necessary
-      }
-      returnAddress = ((uintptr_t)current + HEADER_SIZE);
+        if (alignSize > REQ_SIZE) { // Default size not big enough
+            current = (FreeBlock*)malloc(alignSize); // Get larger chunk from OS
+            current->size = alignSize;
+            current->next = NULL;
+            totalSize += alignSize;
+            returnAddress = ((uintptr_t)current + HEADER_SIZE);
+        } else {
+            // Get 8K bytes memory from OS
+            current = (FreeBlock*)malloc(REQ_SIZE); 
+            current->size = REQ_SIZE;
+            current->next = NULL;
+            freeList = split(current, alignSize); // Split block if necessary
+            totalSize += REQ_SIZE;
+            returnAddress = ((uintptr_t)current + HEADER_SIZE);
+        }
     } else {
-      current = freeList;
-      // The first block in list is big enough
-      if (freeList->size >= alignSize) {
-	freeList = split(freeList, alignSize);
-	returnAddress = ((uintptr_t)current + HEADER_SIZE);
-	goto END;
-      }
+        current = freeList;
+        // The first block in list is big enough
+        if (freeList->size >= alignSize) {
+
+            freeList = split(freeList, alignSize);
+            returnAddress = ((uintptr_t)current + HEADER_SIZE);
+            goto END;
+        }
         
-      // Loop through free list to find block
-      while (current->next != NULL) {
-	if (current->next->size >= alignSize) {
-	  break;
-	}
-	current = current->next;
-      }
-      // Fail to find a block big enough, need a new block
-      if (!current->next) {
-	// Default size not big enough, request a larger chunk
-	if (alignSize > REQ_SIZE) { 
-	  current = create(current, alignSize);
-	  // Default size big enough, split as needed
-	} else {                    
-	  current = create(current, REQ_SIZE);
-	  FreeBlock* remainder = split(current, alignSize);
-	  
-	  // Insert the remainder block to the front
-	  if (remainder < freeList || !freeList) {
-	    remainder->next = freeList;
-	    freeList = remainder;
-	    // Insert the remainder block to the right position
-	  } else {
-	    FreeBlock* temp = freeList;
-	    // Look for correct position to insert
-	    while (temp->next && remainder > temp->next) {
-	      temp = temp->next;
-	    }
-	    // Insert
-	    remainder->next = temp->next;
-	    temp->next = remainder;
-	  }
-	}
-	returnAddress = ((uintptr_t)current + HEADER_SIZE);
-      } else { // Found a good size block in free list
-	FreeBlock* temp = current->next;
-	current->next = split(current->next, alignSize);
-	returnAddress = ((uintptr_t)temp + HEADER_SIZE);
-      }
+        // Loop through free list to find block
+        while (current->next != NULL) {
+            if (current->next->size >= alignSize) {
+                break;
+            }
+            current = current->next;
+        }
+        // Fail to find a block big enough, need a new block
+        if (!current->next) {
+            // Default size not big enough, request a larger chunk
+            if (alignSize > REQ_SIZE) { 
+                current = (FreeBlock*)malloc(alignSize);
+                current->size = alignSize;
+                current->next = NULL;
+                totalSize += alignSize;
+                returnAddress = ((uintptr_t)current + HEADER_SIZE);
+            // Default size big enough, split as needed
+            } else {                    
+                current = (FreeBlock*)malloc(REQ_SIZE);
+                current->size = REQ_SIZE;
+                current->next = NULL;
+                totalSize += REQ_SIZE;
+                FreeBlock* remainder = split(current, alignSize);
+                
+                // Did not split
+                if (!remainder) {
+                    returnAddress = ((uintptr_t)current + HEADER_SIZE);
+                // Insert the remainder block to the front
+                } else if (remainder < freeList || !freeList) {
+                    remainder->next = freeList;
+                    freeList = remainder;
+                    returnAddress = ((uintptr_t)current + HEADER_SIZE);
+                // Insert the remainder block to the right position
+                } else {
+                    FreeBlock* temp = freeList;
+                    // Look for correct position to insert
+                    while (temp->next && remainder > temp->next) {
+                        temp = temp->next;
+                    }
+                    // Insert
+                    remainder->next = temp->next;
+                    temp->next = remainder;
+                    returnAddress = ((uintptr_t)current + HEADER_SIZE);
+                }
+            }
+        } else { // Found a good size block in free list
+            FreeBlock* temp = current->next;
+            current->next = split(current->next, alignSize);
+            returnAddress = ((uintptr_t)temp + HEADER_SIZE);
+        }
     }
- END:
+    END:
     check_heap();
     return (void*)returnAddress;
 }
 
-FreeBlock* create(FreeBlock* block, uintptr_t size) {
-  block = (FreeBlock*)malloc(size);
-  block->size = size;
-  block->next = NULL;
-  totalSize += size;
-  return block;
-}
-
 FreeBlock* split(FreeBlock* block, uintptr_t size) {
-  // Remaining block greater than threshold
-  if (block->size - size >= TH) {
-    FreeBlock* current = block;
-    // Set header for remaining block
-
-    block = (FreeBlock*)((uintptr_t)current + size);
-    block->size = current->size - size;
-    block->next = current->next;
-
-    // Set current block ready for client
-    current->size = size;
-    current->next = NULL;
-    return block;
-  }
-  return block->next;
+    // Remaining block greater than threshold
+    if (block->size - size >= TH) {
+        FreeBlock* current = block;
+        // Set header for remaining block
+        block = (FreeBlock*)((uintptr_t)current + size);
+        block->size = current->size - size;
+        block->next = current->next;
+        // Set current block ready for client
+        current->size = size;
+        current->next = NULL;
+        return block;
+    }
+    return block->next;
 }
